81451ad0f925963cdb4a2fc5f82c73b1b3bb9a8e
franknfjr <frankdonascimentojr@gmail.com>
2024-03-24 17:29:37 -0300

    first commit

---
+++ lib/liveview_styling.ex
@@ -0,0 +1,18 @@
+defmodule LiveviewStyling do
+  @moduledoc """
+  Documentation for `LiveviewStyling`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiveviewStyling.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/liveview_styling_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveviewStylingTest do
+  use ExUnit.Case
+  doctest LiveviewStyling
+
+  test "greets the world" do
+    assert LiveviewStyling.hello() == :world
+  end
+end
